c88ab8b9c5ea5a2d5c860287f7dbef990ea57a1a
MartinezRinaudo <84363516+MartinezRinaudo@users.noreply.github.com>
2023-03-15 14:02:16 -0300

    Routing

--- index.js
+++ index.js
@@ -1,13 +1,46 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req,res) => {
-  res.send('Hello World');
-})
+app.use(express.json());
 
-app.get('/about', (req, res) => {
-  res.send('About');
-})
+const message = '200 OK';
+const fnHttpGet =  (req, res) => {
+  //res.send(message);
+  res.json({
+    "username": "JohnnyB",
+    "password":"1234"
+  });
+}
+const fnHttpPostI =  (req, res) => {
+  console.log(req.body)
+  res.send(message);
+}
+const fnHttpPostII =  (req, res) => {
+  console.log(req.body)
+  console.log(req.params)
+  res.send(`Parametro id: ${id}`)
+  res.send(message);
+}
+
+app.all("/user", (req,res,next) => {
+  console.log("Funcion All - Se llama en todas en las rutas");
+  next();
+});
+
+/* HTTP METHODS:
+  app.post('/route', callback);
+  app.put('/route', callback);
+  app.delete('/route', callback);
+*/
+
+app.get('/', fnHttpGet);
+
+app.get('/about', fnHttpGet);
+
+app.post('/user', fnHttpPostI);
+app.get('/user/:id', fnHttpPostII);
+
+
 
 app.listen(3000, () => {
   console.log("Server running")
