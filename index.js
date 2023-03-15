1bddb5e86dd2a2566f16fcc8636b84285f2f6062
MartinezRinaudo <84363516+MartinezRinaudo@users.noreply.github.com>
2023-03-15 14:49:21 -0300

    Middleware, settings and ejs

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
-app.use(express.json());
+//Settings
+app.set('appName', 'Express JS');
+app.set('port', 3000);
+app.set('view engine', 'ejs');
 
+/*
+function logger(req, res, next) {
+  console.log("Request received");
+  next();
+}
+*/
+
+app.use(express.json()); //Middleware que se utiliza para leer json
+//app.use(logger); //Middleware se ejecuta antes de una peticion
+app.use(morgan('dev'));
+
+
+/*------------------------------------------------------------------------------------*/
 const message = '200 OK';
 const fnHttpGet =  (req, res) => {
   //res.send(message);
@@ -40,8 +57,17 @@ app.get('/about', fnHttpGet);
 app.post('/user', fnHttpPostI);
 app.get('/user/:id', fnHttpPostII);
 
+app.get('/pogues', (req,res) =>{
+  const data = [{name:"Johnny B"},{name:"JJ"},{name:"Pope"}, {name:"Kiara"}];
+  res.render('index.ejs', {pogues:data});
+})
+
+app.use(express.static('public'));
 
 
-app.listen(3000, () => {
-  console.log("Server running")
+app.listen(app.get('port'), () => {
+  console.log(`Server running ${app.get('port')}`);
 });
+
+/*------------------------------------------------------------------------------------*/
+
