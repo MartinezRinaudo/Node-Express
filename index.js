27a48b20803a55c32278db487ace1f65370fe9d0
MartinezRinaudo <84363516+MartinezRinaudo@users.noreply.github.com>
2023-03-15 13:08:55 -0300

    1 file modified

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get('/', (req,res) => {
   res.send('Hello World');
 })
 
+app.get('/about', (req, res) => {
+  res.send('About');
+})
+
 app.listen(3000, () => {
   console.log("Server running")
 });
